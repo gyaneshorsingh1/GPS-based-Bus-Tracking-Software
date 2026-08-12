6a0344064009c5fb2d629efc508ae3a51f9e6d42
Manojhajam <manojhajam4@gmail.com>
2026-08-12 11:33:22 +0545

    Add Parent controllers and routes for dashboard, children, and bus information

---
+++ app/Http/Controllers/Api/V1/Parent/ParentBusController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Parent;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ParentBusController extends Controller
+{
+    //
+}
--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,9 @@ use App\Http\Controllers\Api\V1\Driver\DriverBusController;
 use App\Http\Controllers\Api\V1\Driver\DriverDashboardController;
 use App\Http\Controllers\Api\V1\Driver\DriverLiveTrackingController;
 use App\Http\Controllers\Api\V1\Driver\DriverProfileController;
+use App\Http\Controllers\Api\V1\Parent\ParentBusController;
+use App\Http\Controllers\Api\V1\Parent\ParentChildController;
+use App\Http\Controllers\Api\V1\Parent\ParentDashboardController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -27,11 +30,20 @@ Route::prefix('v1')->group(function () {
             Route::get('/buses/{bus}/students', [DriverBusController::class, 'students']);
             Route::get('/live-tracking', [DriverLiveTrackingController::class, 'index']);
 
-        Route::prefix('attendances')->group(function () {
+            Route::prefix('attendances')->group(function () {
                 Route::get('/', [DriverAttendanceController::class, 'index']);
                 Route::get('/history', [DriverAttendanceController::class, 'history']);
                 Route::post('/mark', [DriverAttendanceController::class, 'markAttendance']);
             });
+        });
+
+        Route::prefix('parent')->middleware('role:Parent')->group(function () {
+
+            Route::get('/dashboard', [ParentDashboardController::class, 'index']);
+            Route::get('/profile', [ParentDashboardController::class, 'profile']);
+            Route::get('/children', [ParentChildController::class, 'index']);
+            Route::get('/children/{student}', [ParentChildController::class, 'show']);
+            Route::get('/children/{student}/bus', [ParentBusController::class, 'show']);
         });
     });
 });
